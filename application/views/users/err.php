534f235202056b30bd990030a6e06684de301763
gamecmt <gamecmt@126.com>
2016-01-09 12:29:42 +0800

    Revert "midcms from codeigniter"
    This reverts commit 1014980b759dd7c1c186635c4f35258601c36be9.

--- application/views/users/err.php
+++
@@ -1,5 +0,0 @@
-<p>$sql_password:<?php echo $sql_password; ?></p>
-<p>$old_password:<?php echo $old_password; ?></p>
-<p>$encripted_old_pass:<?php echo $encripted_old_pass; ?></p>
-<p>$password:<?php echo $password; ?></p>
-<p>$encripted_pass:<?php echo $encripted_pass; ?></p>
