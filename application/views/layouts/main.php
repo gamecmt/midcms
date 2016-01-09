ac681715c88ff04a5599db3be3301d463a2f1614
gamecmt <gamecmt@126.com>
2016-01-09 12:32:47 +0800

    Revert "Revert "midcms from codeigniter""
    This reverts commit 534f235202056b30bd990030a6e06684de301763.

---
+++ application/views/layouts/main.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/custom.css">
+	<script src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
+	<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+	<title>MIDCMS</title>
+</head>
+
+<body>
+
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo base_url(); ?>">MIDCMS</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="<?php echo base_url(); ?>">首页 <span class="sr-only">(current)</span></a></li>
+        <?php if ($this->session->userdata('logged_in')): ?>
+        <li><a href="<?php echo base_url(); ?>projects">Projects </a></li>
+        <?php endif; ?>
+        <li><a href="<?php echo base_url(); ?>users/register">注册 </a></li>
+      </ul>
+      <?php if($this->session->userdata('logged_in')): ?>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="<?php echo base_url(); ?>users/logout">退出</a></li>
+      </ul>
+      <?php endif;?>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<div class="container">
+	<div class="col-xs-3">
+		
+		<?php $this->load->view('users/login_view'); ?>
+
+	</div>
+	<div class="col-xs-9">
+
+		<?php $this->load->view($main_view); ?>
+
+	</div>
+</div>
+</body>
+</html>
---
+++ application/views/users/login_view.php
@@ -0,0 +1,78 @@
+<?php if ($this->session->userdata('logged_in')): ?>
+
+<?php echo form_open('users/logout'); ?>
+
+<h2>退出</h2>
+
+<p>
+<?php if ($this->session->userdata('username')): ?>
+<p><?php echo "欢迎光临， " . $this->session->userdata('username') . "。<br>"; ?></p>
+<p><a href="<?php echo base_url(); ?>users/edit_info/<?php echo $this->session->userdata('id'); ?>">修改账号</a></p>
+<p><a href="<?php echo base_url(); ?>users/edit_password/<?php echo $this->session->userdata('id'); ?>">修改密码</a></p>
+<?php endif; ?>
+</p>
+
+<?php
+	$data = array(
+		'name' => 'submit',
+		'class' => 'btn btn-primary',
+		'value' => '退出登录'
+	);
+	echo form_submit($data);
+	echo form_close(); 
+?>
+
+<?php else: ?>
+
+<h2>登录</h2>
+
+<?php 
+	if ($this->session->flashdata('errors')){
+		echo $this->session->flashdata('errors');		
+	}
+
+	$attributes = array('id'=>'login_form', 'class'=>'form_horizontal');
+	echo form_open('users/login', $attributes); 
+?>
+
+<div class="form-group">
+<?php 
+	echo form_label("账户");
+
+	$data = array(
+		'class'=>'form-control',
+		'name'=>'username',
+		'placeholder'=>"输入账户"
+	);
+
+	echo form_input($data); 
+?>
+</div>
+
+<div class="form-group">
+<?php 
+	echo form_label('密码');
+
+	$data = array(
+		'class'=>'form-control',
+		'name'=>'password',
+		'placeholder'=>'输入密码'
+	);
+
+	echo form_password($data); ?>
+</div>
+
+<div class="form-group">
+<?php
+	$data = array(
+		'class'=>'btn btn-primary',
+		'name'=>'submit',
+		'value'=>'登录'
+	);
+?>
+<?php echo form_submit($data); ?>
+</div>
+
+<?php echo form_close(); ?>
+
+<?php endif; ?>
